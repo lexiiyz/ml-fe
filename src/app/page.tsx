43ac9c4ca6cdad1b6b70aab90466266e2d3c0163
lexiiyz <radityarakha01@gmail.com>
2025-12-02 23:12:18 +0700

    ui enchantments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,9 +73,9 @@ export default function Home() {
     };
 
     return (
-        <div className="bg-gradient-to-br from-yellow-500 via-green-400 to-blue-50 min-h-screen pt-28">
+        <div className="bg-linear-to-br from-yellow-500 via-green-400 to-blue-50 min-h-screen pt-28">
             <Navbar />
-            <main className="flex-grow w-full max-w-4xl mx-auto p-4 sm:p-8 my-5 sm:my-10 bg-white rounded-2xl shadow-2xl border border-gray-100 text-center text-gray-800">
+            <main className="grow w-full max-w-4xl mx-auto p-4 sm:p-8 my-5 sm:my-10 bg-white rounded-2xl shadow-2xl border border-gray-100 text-center text-gray-800">
 
                 <h1 className="text-2xl sm:text-3xl font-extrabold text-green-700 mb-2">
                     Kenali Buah & Sayurmu 🍎🥦
